761904701fa3a5070aeaf259fe22adadc788939b
Kyle Deak <kdeak1210@gmail.com>
2018-04-03 12:30:48 -0400

    Added devtool object to webpack config
    value 'source-map' for devtool has a slow rebuild speed, but offers the best debugging with original source code. See: https://webpack.js.org/configuration/devtool/

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    sourceMapFilename: 'index_bundle.map'
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
